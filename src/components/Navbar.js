2577ec879f8acec3d817bc48d75163d11ee6718a
Gikonyo-Otieno <gikonyootieno@gmail.com>
2023-01-27 02:45:04 +0300

    Making Navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,15 @@
+import { Component} from 'react'
 import './NavbarStyles.css';
 
 
 
-function Navbar(){
+class Navbar extends Component{
+
+   state = {clicked: false};
+   handleClick = () => {
+    this.setState({clicked: !this.state.clicked});
+   };
+   render() {
     return (
         <>
             <nav>
@@ -13,13 +20,19 @@ function Navbar(){
                 </a>
 
                 <div>
-                    <ul id="navbar">
+                    <ul id="navbar" className={this.state.clicked ?
+                    "navbar active" : "navbar"}>
                         <li><a className="active" href="index.html">Home</a></li>
                         <li><a href="index.html">Shop</a></li>
                         <li><a href="index.html">Blog</a></li>
                         <li><a href="index.html">About</a></li>
                         <li><a href="index.html">Contact</a></li>
                     </ul>
+                </div>
+
+                <div id="mobile" onClick={this.handleClick}>
+                    <i id="bar" className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                    
                 </div>
             </nav>
 
@@ -28,6 +41,6 @@ function Navbar(){
         </>
     )
 }
-
+}
 
 export default Navbar;
