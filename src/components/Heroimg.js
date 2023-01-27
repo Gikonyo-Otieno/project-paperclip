03e52e2b61a8a9ee050bef16b2fb37804675f606
Gikonyo-Otieno <gikonyootieno@gmail.com>
2023-01-27 07:45:14 +0300

    Hero and starting footer

--- src/components/Heroimg.js
+++ src/components/Heroimg.js
@@ -1,12 +1,22 @@
 import './HeroimgStyles.css'
 
  import React from 'react'
-
+ import IntroImg from '../assets/IntroImg.jpg'
+import {Link} from 'react-router-dom'
  const Heroimg = () => {
     return (
         <div className = "hero">
             <div className = "mask">
-                
+                <img className= "into-img" src = {IntroImg} alt=""/>
+
+            </div>
+            <div className="content">
+                <p>YOUR ONE STOP SHOP FOR TICKETS</p>
+                <h1>Find what you need.</h1>
+                <div>
+                    <Link to="/project" className= "btn">Movies</Link>
+                    <Link to="/contact" className= "btn btn-light">Sign Up</Link>
+                </div>
             </div>
         </div>
     )
